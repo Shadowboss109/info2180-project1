56e395b3fad9aacb5badc9b6af34f951e8183a48
Theodore <theodorebennett3@gmail.com>
2021-10-24 21:34:05 -0500

    Edited javascript to validate email

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-/* Add your JavaScript to this file */
+window.addEventListener('load', (event) => {
+
+
+  
+
+  var btn = document.querySelector(" button");
+
+  btn.addEventListener("click", e =>{
+    e.preventDefault();
+
+      var address = document.getElementById("email").value;
+
+      var message = document.getElementsByClassName("message");
+
+      if (  address === "") {
+          
+
+
+        
+
+          message[0].innerText = "Please enter your email address.";     
+      }
+      else{
+
+          message[0].innerText = "Thank you! Your email address " + address+" has been added to our mailing list!";     
+
+      }
+
+  });
+
+
+
+});
